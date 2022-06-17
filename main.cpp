b31fab96cefa15ad0fc44a1131b0b38986e1370c
Burma Emmanuel <bemmanue@un-e3.kzn.21-school.ru>
2022-06-17 17:59:44 +0300

    Insert in process

--- main.cpp
+++ main.cpp
@@ -4,22 +4,22 @@
 
 int main()
 {
-	ft::vector<int> b(1);
+	ft::vector<int> b(5);
 	b[0] = 1;
-	b.pop_back();
-//	b[1] = 5;
-//	b[2] = 2;
-//	b[3] = 3;
-//	b[4] = 4;
-	std::cout << b.size() << std::endl;
-	std::cout << b.capacity() << std::endl << std::endl;
-//	b.clear();
+	b[1] = 5;
+	b[2] = 2;
+	b[3] = 3;
+	b[4] = 4;
+	b.insert(b.begin(), 1, 7);
+	for (int i = 0; i < b.size(); i++)  {
+		std::cout << b[i] << std::endl;
+	}
+	std::cout << "-\n" << b.size() << std::endl;
+	std::cout << b.capacity() << std::endl;
 //
 //	std::cout << b.size() << std::endl;
 //	std::cout << b.capacity() << std::endl;
-//	for (int i = 0; i < b.size(); i++)  {
-//		std::cout << b[i] << std::endl;
-//	}
+
 //	std::cout << *(v.end() - 1) << std::endl;
 //	std::cout << v.size() << std::endl;
 //	std::cout << v.capacity() << std::endl;
@@ -27,21 +27,21 @@ int main()
 //		std::cout << b[i] << std::endl;
 //	}
 
-	std::vector<int> e(1);
-	e[0] = 1;
-	e.pop_back();
+//	std::vector<int> e(5);
+//	e[0] = 1;
 //	e[1] = 5;
 //	e[2] = 2;
 //	e[3] = 3;
 //	e[4] = 4;
-//	std::vector<int>::iterator q = e.begin() + 10;
+//	e.insert(e.end(), 1, 0);
+	//	std::vector<int>::iterator q = e.begin() + 10;
 //	e.insert(q, 9);
 //	e.clear();
 //	for (int i = 0; i < e.size(); i++)  {
 //		std::cout << e[i] << std::endl;
 //	}
-	std::cout << e.size() << std::endl;
-	std::cout << e.capacity() << std::endl;
+//	std::cout << e.size() << std::endl;
+//	std::cout << e.capacity() << std::endl;
 
 return 0;
 }
--- vector/modifiers.hpp
+++ vector/modifiers.hpp
@@ -15,12 +15,36 @@ void vector<T, Alloc>::clear() {
 
 template <typename T, class Alloc>
 typename vector<T, Alloc>::iterator vector<T, Alloc>::insert(iterator pos, const T &value) {
-
+	;
 }
 
 template <typename T, class Alloc>
 void vector<T, Alloc>::insert(iterator pos, size_type count, const T &value) {
+	size_t max_size = this->max_size();
+	size_t capacity = this->capacity();
+	size_t size = this->size();
 
+	if (count == 0) {
+		;
+	} else if (count > max_size - size) {
+		throw std::length_error("vector");
+	} else if ((size + count) < capacity) {
+		size_t new_capacity = (max_size - (capacity / 2)) < capacity ? 0 : (capacity + (capacity / 2));
+		if (new_capacity < size + count)
+			new_capacity = size + count;
+		pointer newPointer = _allocator.allocate(new_capacity, nullptr);
+		pointer newLastPointer;
+		newLastPointer = copy(newPointer, begin(), end());
+		newLastPointer = fill(newLastPointer, count, value);
+		if (_first) {
+			clean();
+		}
+		_end = newPointer + new_capacity;
+		_last = newPointer + size + count;
+		_first = newPointer;
+	} else {
+		;
+	}
 }
 
 template <typename T, class Alloc>
